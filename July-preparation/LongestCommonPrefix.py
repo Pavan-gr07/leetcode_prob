3f4c35cc2b0b6a2e41e7d8792589b23401e146ea
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2025-07-16 10:51:46 +0530

    longestCommon prefix and sum fixes

--- July-preparation/LongestCommonPrefix.py
+++ July-preparation/LongestCommonPrefix.py
@@ -1,9 +1,11 @@
-def commonPrefix():
-    strs = ["flower","flow","flight"]
-    first = strs[0].split
-    print(first)
-    
+def commonPrefix(strs):
+    ans = ""
+    for i in range(len(strs[0])):
+        for j in range(1,len(strs)):
+            if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                return strs[0][:i]
+    return strs[0]
 
-print(commonPrefix())
+print(commonPrefix( ["ab", "a"]))
 
     
